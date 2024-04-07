1d09e9beddb5cd5a5385a3de05e38978a0183674
Gulshat <gulshatkhamidulla313@gmail.com>
2024-04-07 20:47:11 +0500

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldPostToPostmanEcho() {
+
+    // Given - When - Then
+    // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("The topic of the meeting")
+                // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+    // Выполняемые действия
+        .when()
+                .post("/post")
+
+    // Проверки
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("The topic of the meeting"))
+                .body("headers.content-length", equalTo("24"));
+        ;
+    }
+}
